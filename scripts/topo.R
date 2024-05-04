cce7072b7be6f1a6e3202d60318400a6856457ea
rupert@jau <rupertcollins@gmail.com>
2024-05-04 20:58:12 +0100

    work on terrain

--- scripts/topo.R
+++ scripts/topo.R
@@ -8,7 +8,10 @@ library("elevatr")
 library("terra")
 library("marmap")
 library("giscoR")
-library("rayshader")
+library("terra")
+library("tidyterra")
+library("scales")
+#library("rayshader")
 
 ### BASEMAP ###
 
@@ -56,13 +59,6 @@ rivers.filt <- rivers.crop |> dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |>
             width == 6 ~ 1)
         )
 
-rivers.filt <- rivers.crop |> dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |> 
-    mutate(width=as.numeric(ORD_FLOW), 
-        width=flipper(width)
-        )
-
-
-
 
 # flip fun
 flipper <- function(x){
@@ -71,49 +67,104 @@ flipper <- function(x){
 }
 
 
+rivers.filt <- rivers.crop |> dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |> 
+    mutate(width=as.numeric(ORD_FLOW), 
+        width=flipper(width)
+        )
+
+
+
+
+
 
 ### TOPOGRAPHY ###
 
 #  get raster
 get_elevation_data <- function(country,res) {
     country.elevation <- elevatr::get_elev_raster(locations = country, z = res, clip = "bbox") 
-    country.elevation.df <- as.data.frame(country.elevation, xy = T) |> na.omit()
-    colnames(country.elevation.df)[3] <- "elevation"
-    return(country.elevation.df)
+    return(country.elevation)
 }
 
-country.elevation.df <- get_elevation_data(country=country.region,res=5)
+country.elevation <- get_elevation_data(country=country.region,res=5)
+
+# turn raster into df 
+country.elevation.df <- as.data.frame(country.elevation, xy = T) |> na.omit()
+colnames(country.elevation.df)[3] <- "elevation"
+
 
 
 ### SHADE ###
+# https://dieghernan.github.io/202210_tidyterra-hillshade/
+# https://dominicroye.github.io/en/2022/hillshade-effects/
 
-country.matrix <-elevatr::get_elev_raster(locations = country.region, z = 5, clip = "bbox") |> rayshader::raster_to_matrix()
+country.elevation.spat <- terra::rast(country.elevation)
 
-ambmat <- ambient_shade(country.matrix, zscale = 30)
-raymat <- ray_shade(country.matrix, zscale = 30, lambert = TRUE)
-watermap <- detect_water(country.matrix)
 
-country.matrix %>%
-  sphere_shade() %>%
-  add_water(detect_water(country.matrix)) %>%
-  plot_map()
+## Create hillshade effect
 
-p |> rayshader::plot_gg(preview=TRUE)
+slope <- terra::terrain(country.elevation.spat, "slope", unit = "radians")
+aspect <- terra::terrain(country.elevation.spat, "aspect", unit = "radians")
+hill <- terra::shade(slope, aspect, 30, 270)#
+
+# normalize names
+names(hill) <- "shades"
+
+# Hillshading, but we need a palette
+pal_greys <- hcl.colors(1000, "Grays")
+#show_col(pal_greys)
+
+
+addAlpha <- function(COLORS, ALPHA){
+    if(missing(ALPHA)) stop("provide a value for alpha between 0 and 1")
+    RGB <- col2rgb(COLORS, alpha=TRUE)
+    RGB[4,] <- round(RGB[4,]*ALPHA)
+    NEW.COLORS <- rgb(RGB[1,], RGB[2,], RGB[3,], RGB[4,], maxColorValue = 255)
+    return(NEW.COLORS)
+}
+
+
+pal.greys.alph <- addAlpha(pal_greys,seq.int(1, 0, length.out=1000))
+#show_col(pal.greys.alph)
+
+index <- hill %>%
+  mutate(index_col = scales::rescale(shades, to = c(1, length(pal_greys)))) %>%
+  mutate(index_col = round(index_col)) %>%
+  pull(index_col)
+
+
+# Get cols
+vector_cols <- pal_greys[index]
+
+# Get cols
+vector_cols <- pal.greys.alph[index]
+
+
+ggplot() +
+    geom_spatraster(data = country.elevation.spat, maxcell = Inf, alpha=1) +
+    scale_fill_etopo() +
+    geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width)) +
+    scale_linewidth(range=c(0.1,0.8)) +
+    geom_spatraster(data = hill, fill = vector_cols, maxcell = Inf, alpha = 0.3) + 
+    geom_sf(data=country.region,alpha=0,color="white",linewidth=0.2) +
+    theme_minimal()
+    
+
+
 
 # PLOT
 
 p <- ggplot() +
     geom_tile(data = country.elevation.df, aes(x=x,y=y,fill=elevation)) + #, alpha=0.95
+    #tidyterra::scale_fill_hypso_tint_c() +
     scale_fill_etopo() +
-    geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width)) +
-    scale_linewidth(range=c(0.1,0.6)) +
-    scale_alpha(range=c(0.3,0.8)) +
+    #geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width)) +
+    #scale_linewidth(range=c(0.1,0.6)) +
+    #scale_alpha(range=c(0.3,0.8)) +
     geom_sf(data=country.region,alpha=0,color="white",linewidth=0.2) +
     #geom_sf(data=lakes,fill="blue",color="blue") +
     #coord_sf(xlim = c(90,102), ylim = c(9,30), expand = FALSE, crs=crsLONGLAT) +
     theme_bw() +
     theme(legend.position="none",panel.grid = element_blank())#
-
 plot(p)
 
 ggsave(filename=here("temp/topo_map.png"), width=9, height=9, dpi = 300, device='png', plot=p)
