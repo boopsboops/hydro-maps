971e6894c03b68414570004e8cd0b210773c2054
rupert@jau <rupertcollins@gmail.com>
2024-05-05 20:22:53 +0100

    rename

--- scripts/generate-topo-map.R
+++ scripts/generate-topo-map.R
@@ -1,95 +1,45 @@
 #!/usr/bin/env Rscript
 
-# load libs
-library("here")
-library("tidyverse")
-library("sf")
-library("elevatr")
-library("terra")
-library("marmap")
-library("giscoR")
-library("terra")
-library("tidyterra")
-library("scales")
-#library("rayshader")
+# load libs and funs
+source(here::here("scripts/load-libs.R"))
+
+
 
 ### BASEMAP ###
 
-# set lonlat and proj
-crsLONGLAT <- "+proj=longlat +datum=WGS84 +no_defs"
-
-# test a country
-#giscoR::gisco_get_countries(country="Myanmar") |> sf::st_geometry() |> plot(col = "seagreen2")
-
-# fun to get country
-get_sf <- function(country) {
-    country.sf <- giscoR::gisco_get_countries(resolution = "10",country = country)
-    country.transformed <- sf::st_transform(country.sf, crs = crsLONGLAT)
-    return(country.transformed)
-}
-
-country.focus <- get_sf(country="Myanmar")
-country.neighbours <- get_sf(country=c("Myanmar","Thailand","India","Nepal","Laos","Bangladesh","China"))
-
+# get basemap using gisco_get_countries() and crop to focus country
+# res: One of "60" (low res), "20" , "10", "03" or "01" (high res).
+# year: One of "2001", "2006", "2010", "2013", "2016" or "2020"
+country.focus <- get_country_region(country="Myanmar",resolution="03",year="2020")
+country.neighbours <- get_country_region(country=c("Myanmar","Thailand","India","Nepal","Laos","Bangladesh","China"),resolution="03",year="2020")
 country.region <- sf::st_crop(country.neighbours, country.focus)
+
 
 
 ### RIVERS ###
 
-# get rivers
-#download.file(url="https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_as_shp.zip",destfile=here("assets/HydroRIVERS_v10_as_shp.zip"))
-
-# unzip
-#unzip(zipfile=here("assets/HydroRIVERS_v10_as_shp.zip"),exdir=here("assets"))
-
-# load shapefile
-rivers <- sf::st_read("assets/HydroRIVERS_v10_as_shp/HydroRIVERS_v10_as.shp")
+# download (or load from file) from hydrosheds
+rivers <- get_hydrosheds_rivers(url="https://data.hydrosheds.org/file/HydroRIVERS/HydroRIVERS_v10_as_shp.zip") 
 
 # crop to country
 rivers.crop <- sf::st_crop(rivers,country.region)
 
-# filter large rivers and make thickness col
-rivers.filt <- rivers.crop |> dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |> 
-    mutate(width=as.numeric(ORD_FLOW), 
-        width=case_when(width == 1 ~ 6, 
-            width == 2 ~ 5, 
-            width == 3 ~ 4, 
-            width == 4 ~ 3, 
-            width == 5 ~ 2,
-            width == 6 ~ 1)
-        )
+# filter on ORD_STRA (stream order) and ORD_FLOW (river flow)
+rivers.crop |> st_drop_geometry() |> tibble() |> group_by(ORD_STRA) |> count()
+rivers.crop |> st_drop_geometry() |> tibble() |> group_by(ORD_FLOW) |> count()
 
-
-# flip fun
-flipper <- function(x){
-    y <- (max(x) - x) + min(x)
-    return(y)
-}
-
-
-rivers.filt <- rivers.crop |> dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |> 
-    mutate(width=as.numeric(ORD_FLOW), 
-        width=flipper(width)
-        )
-
-
-
+# filter
+rivers.filt <- rivers.crop |> 
+    dplyr::filter(ORD_STRA >= 4 & ORD_FLOW <= 6) |> 
+    dplyr::mutate(width=as.numeric(ORD_FLOW), width=flipper(width))
 
 
 
 ### TOPOGRAPHY ###
 
-#  get raster
-get_elevation_data <- function(country,res) {
-    country.elevation <- elevatr::get_elev_raster(locations = country, z = res, clip = "bbox") 
-    return(country.elevation)
-}
-
-country.elevation <- get_elevation_data(country=country.region,res=5)
-
-# turn raster into df 
-country.elevation.df <- as.data.frame(country.elevation, xy = T) |> na.omit()
-colnames(country.elevation.df)[3] <- "elevation"
+# get topo from elevatr
+# z = zoom level to return, ranges from 1 (low) to 14 (high) 
+country.elevation <- elevatr::get_elev_raster(locations=country.region,clip="bbox",z=8)# rough 5, final 8
 
 
 
@@ -97,74 +47,75 @@ colnames(country.elevation.df)[3] <- "elevation"
 # https://dieghernan.github.io/202210_tidyterra-hillshade/
 # https://dominicroye.github.io/en/2022/hillshade-effects/
 
-country.elevation.spat <- terra::rast(country.elevation)
+# convert to spatrast 
+country.elevation.spat <- terra::rast(country.elevation) 
 
+# and remove bathymetry
+names(country.elevation.spat) <- "alt"
+country.elevation.spat <- country.elevation.spat |> dplyr::mutate(alt=pmax(-10,alt))
 
 ## Create hillshade effect
-
-slope <- terra::terrain(country.elevation.spat, "slope", unit = "radians")
-aspect <- terra::terrain(country.elevation.spat, "aspect", unit = "radians")
-hill <- terra::shade(slope, aspect, 30, 270)#
+slope <- terra::terrain(country.elevation.spat,"slope",unit="radians")
+aspect <- terra::terrain(country.elevation.spat,"aspect",unit ="radians")
+hill <- terra::shade(slope,aspect,30,270)
 
 # normalize names
 names(hill) <- "shades"
 
-# Hillshading, but we need a palette
-pal_greys <- hcl.colors(1000, "Grays")
-#show_col(pal_greys)
+# hillshading palette
+pal.greys <- hcl.colors(1000, "Grays")
+#show_col(pal_greys,labels=FALSE)
 
-
-addAlpha <- function(COLORS, ALPHA){
-    if(missing(ALPHA)) stop("provide a value for alpha between 0 and 1")
-    RGB <- col2rgb(COLORS, alpha=TRUE)
-    RGB[4,] <- round(RGB[4,]*ALPHA)
-    NEW.COLORS <- rgb(RGB[1,], RGB[2,], RGB[3,], RGB[4,], maxColorValue = 255)
-    return(NEW.COLORS)
-}
-
-
-pal.greys.alph <- addAlpha(pal_greys,seq.int(1, 0, length.out=1000))
-#show_col(pal.greys.alph)
-
-index <- hill %>%
-  mutate(index_col = scales::rescale(shades, to = c(1, length(pal_greys)))) %>%
-  mutate(index_col = round(index_col)) %>%
-  pull(index_col)
-
+# rescale shades
+index <- hill |>
+    dplyr::mutate(index_col=scales::rescale(shades,to=c(1,length(pal.greys)))) |>
+    dplyr::mutate(index_col=round(index_col)) |>
+    dplyr::pull(index_col)
 
 # Get cols
-vector_cols <- pal_greys[index]
-
-# Get cols
-vector_cols <- pal.greys.alph[index]
+vector.cols <- pal.greys[index]
 
 
-ggplot() +
-    geom_spatraster(data = country.elevation.spat, maxcell = Inf, alpha=1) +
+
+### ADD DATA ###
+
+# load  and filter
+myanmar.locations <- read_csv(here("assets/myanmar-locations.csv"),show_col_types=FALSE)
+
+myanmar.locations.distinct <- myanmar.locations |> 
+    distinct(decimalLatitude,decimalLongitude) |> 
+    filter(!is.na(decimalLatitude) & !is.na(decimalLongitude)) |>
+    filter(decimalLongitude > 90 & decimalLatitude > 11) |>
+    st_as_sf(coords=c("decimalLongitude","decimalLatitude"),crs=4326)
+
+
+
+### PLOT ###
+
+# make baseplot
+baseplot <- ggplot() +
+    geom_spatraster(data=country.elevation.spat,maxcell=Inf,alpha=1) +
     scale_fill_etopo() +
-    geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width)) +
-    scale_linewidth(range=c(0.1,0.8)) +
-    geom_spatraster(data = hill, fill = vector_cols, maxcell = Inf, alpha = 0.3) + 
-    geom_sf(data=country.region,alpha=0,color="white",linewidth=0.2) +
-    theme_minimal()
-    
+    geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width),lineend="round",linejoin="round") +
+    scale_linewidth(range=c(0.1,0.6)) +
+    geom_spatraster(data=hill,fill=vector.cols,maxcell=Inf,alpha=0.2) + 
+    geom_sf(data=country.region,alpha=0,color="white",linewidth=0.1) +
+    geom_sf(data=myanmar.locations.distinct,shape=24,fill="grey90",color="grey30",size=3.5)
 
+#plot(baseplot)
+#ggsave(filename=here("temp/myanmar.png"),width=210,height=297,units="mm",dpi=300,device="png",plot=baseplot)
 
+# pretty plot
+prettyplot <- baseplot + 
+    theme_minimal() +
+    #xlim(c(93,100)) +
+    theme(legend.position="none",
+        panel.grid=element_line(color="grey30",linewidth=0.05),
+        plot.background=element_rect("grey97",colour=NA)
+        ) +
+    scale_y_continuous(expand=c(0.01,0.01)) +
+    scale_x_continuous(expand=c(0.01,0.01)) +
+    ggspatial::annotation_scale()
 
-# PLOT
-
-p <- ggplot() +
-    geom_tile(data = country.elevation.df, aes(x=x,y=y,fill=elevation)) + #, alpha=0.95
-    #tidyterra::scale_fill_hypso_tint_c() +
-    scale_fill_etopo() +
-    #geom_sf(data=rivers.filt,color="#0d4a70",aes(linewidth=width,alpha=width)) +
-    #scale_linewidth(range=c(0.1,0.6)) +
-    #scale_alpha(range=c(0.3,0.8)) +
-    geom_sf(data=country.region,alpha=0,color="white",linewidth=0.2) +
-    #geom_sf(data=lakes,fill="blue",color="blue") +
-    #coord_sf(xlim = c(90,102), ylim = c(9,30), expand = FALSE, crs=crsLONGLAT) +
-    theme_bw() +
-    theme(legend.position="none",panel.grid = element_blank())#
-plot(p)
-
-ggsave(filename=here("temp/topo_map.png"), width=9, height=9, dpi = 300, device='png', plot=p)
+#plot(prettyplot)
+ggsave(filename=here("temp/myanmar.png"),width=210,height=297,units="mm",dpi=600,device="png",plot=prettyplot)
